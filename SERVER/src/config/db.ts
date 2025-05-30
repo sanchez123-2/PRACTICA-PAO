2c4e65be8bef7f0798ef03a648609b8dbfb952a2
Ramon Ramirez <ramonrrxd@gmail.com>
2025-05-30 00:25:53 -0600

    UPDATE DB

--- SERVER/src/config/db.ts
+++ SERVER/src/config/db.ts
@@ -2,6 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 dotenv.config();
 
+if (!process.env.DB_URL) {
+  throw new Error("La variable de entorno DB_URL no está definida. Verifica tu configuración.");
+}
+
 const db = new Sequelize(process.env.DB_URL!, {
   models: [__dirname + "/../models/**/*.ts"],
   dialectOptions: {
